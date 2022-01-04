31a8d71d0a5354608392cb73060732f617aee56f
= <=>
2022-01-04 09:53:22 +0700

    update

--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,5 +1,13 @@
 const socketIo = (io) => {
   // code here
+  io.on('connection', (socket) => {
+    console.log('client connect :', socket.id);
+
+    socket.on("disconnect", () => {
+      console.log('client disconnect');
+    })
+  })
+
 }
 
 module.exports = socketIo

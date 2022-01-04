95c691fc2288ed3b05f4b2da33e6e7160056d8af
= <=>
2022-01-04 10:53:44 +0700

    update

--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,10 +1,79 @@
 // import models here
+const { chat, user, profile } = require("../../models")
 
 const socketIo = (io) => {
   io.on('connection', (socket) => {
     console.log('client connect: ', socket.id)
 
     // code here
+    socket.on("load admin contact", async () => {
+      try {
+        const adminContact = await user.findOne({
+          include: [
+            {
+              model: profile,
+              as: "profile",
+              attributes: {
+                exclude: ["createdAt", "updatedAt"]
+              }
+            }
+          ],
+          where: {
+            status: "admin"
+          },
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "password"]
+          }
+        })
+
+        socket.emit("admin contact", adminContact)
+      } catch (error) {
+        console.log(error);
+      }
+    })
+
+    socket.on("load customer contacts", async () => {
+      try {
+        let customerContacts = await user.findAll({
+          include: [
+            {
+              model: profile,
+              as: "profile",
+              attributes: {
+                exclude: ["createdAt", "updatedAt"]
+              }
+            },
+            {
+              model: chat,
+              as: "senderMessage",
+              attributes: {
+                exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"]
+              }
+            },
+            {
+              model: chat,
+              as: "recipientMessage",
+              attributes: {
+                exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"]
+              }
+            },
+          ],
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "password"]
+          }
+        })
+
+        customerContacts = JSON.parse(JSON.stringify(customerContacts))
+        customerContacts = customerContacts.map(item => ({
+          ...item,
+          image: item.image ? process.env.PATH_FILE + item.image : null
+        }))
+
+        socket.emit("customer contacts", customerContacts)
+      } catch (error) {
+        console.log(error);
+      }
+    })
 
     socket.on("disconnect", () => {
       console.log("client disconnect")
